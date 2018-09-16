f574b75e80fcc3253ccbbfa429a167b67f14eee4
steph <41528322+st3p4@users.noreply.github.com>
2018-09-16 23:11:28 +1000

    Update TestCollectTreasure.java

--- src/tests/TestCollectTreasure.java
+++ src/tests/TestCollectTreasure.java
@@ -1,8 +1,7 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 import enemies.*;
 import dungeon.*;
@@ -13,8 +12,52 @@ import game.*;
 class TestCollectTreasure {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testLevel02a() {
+		Level level = new Level(TestMaze.LEVEL02);
+		Treasure treasure = new Treasure();
+
+		level.move(Direction.DOWN);
+		assertTrue(level.playerIsAt(2,3));
+
+		level.move(Direction.DOWN);
+		assertTrue(level.playerIsAt(2,4));
+		assertTrue("There is a treasure to pick up",level.itemIsAt(treasure, 2, 4));
+
+		level.getPlayer().pickUp(treasure);
+		assertTrue(level.playerHas(treasure));
+
+		level.move(Direction.LEFT);
+		level.move(Direction.LEFT);
+		level.move(Direction.UP);
+
+		level.getPlayer().pickUp(treasure);
+		assertTrue(level.playerHas(treasure) > 0);
+		assertTrue(playerHas(treasure) > 1);
+
+		level.completeLevel();
+		assertTrue(level.isComplete());
+
 	}
 
+	@Test
+	void testLevel02b() {
+		Level level = new Level(TestMaze.LEVEL02);
+		Treasure treasure = new Treasure();
+
+		level.move(Direction.DOWN);
+		level.move(Direction.LEFT);
+		level.move(Direction.LEFT);
+
+		level.getPlayer().pickUp(treasure);
+
+		assertFalse(level.playerHas(treasure) == 0);
+		assertFalse(level.isComplete());
+
+		level.move(Direction.DOWN);
+		level.getPlayer().pickUp(treasure);
+
+		assertFalse(level.itemIsAt(treasure, 4, 3));
+		assertTrue(level.playerHas(treasure) > 0);
+		assertTrue(level.itemIsAt(treasure, 2, 4));
+	}
 }
