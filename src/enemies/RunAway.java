f42e92399f8da38ecb61ea1651de52a98262352d
James Slack-Smith <jpslacksmith@hotmail.com.au>
2018-10-20 14:49:47 +1100

    implement functional run away, based on SimpleChase

--- src/enemies/RunAway.java
+++ src/enemies/RunAway.java
@@ -20,8 +20,38 @@ public class RunAway implements MovementPattern {
 	
 	@Override
 	public Direction chooseMove(Square s, Maze maze, Direction oldMove) {
-		// TODO Auto-generated method stub
-		return Direction.DOWN;
+		Square src = maze.getPlayerLocation();
+		int[][] distances = maze.getDistances(src, s);
+
+		// Debugging...
+//		for (int i = 0; i < distances.length; i++) {
+//			System.out.print("|");
+//			for (int j = 0; j < distances[0].length; j++) {
+//				System.out.printf("%3d|", distances[i][j]);
+//			}
+//			System.out.print("\n");
+//		}
+
+		int y = s.getY();
+		int x = s.getX();
+		int ys[] = {y - 1, y + 1, y + 0, y + 0};
+		int xs[] = {x + 0, x + 0, x + 1, x - 1};
+
+		int[] adjDistances = new int[4];
+		int maxDist = 1000000;
+		int maxIndex = 0;
+		for (int i = 0; i < 4; i++) {
+			adjDistances[i] = distances[ys[i]][xs[i]] * 10000;
+			adjDistances[i] += ((ys[i] - src.getY()) * (ys[i] - src.getY()) +
+					(xs[i] - src.getX()) * (xs[i] - src.getX()));
+			if (adjDistances[i] > maxDist && adjDistances[i] >= 0) {
+				maxDist = adjDistances[i]; maxIndex = i;
+			}
+		}
+		if (oldMove != null && adjDistances[oldMove.toInt()] == maxDist) {
+			return oldMove;
+		}
+		return Direction.intToDirection(maxIndex);
 	}
 
 }
